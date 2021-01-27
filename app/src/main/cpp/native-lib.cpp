4600620dcd2ce9df615989fb2e411ed597d605ef
Mike-Rush <dddd1d23@163.com>
2021-01-27 22:01:08 +0800

    For Share

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,115 @@
 #include <jni.h>
 #include <string>
+#include <sign_api.h>
+#include <cstring>
+#define PRODUCT_KEY "a1UXTORPgGu"
+#define PRODUCT_SECERT "oZNNZnDa4791TUFj"
+using namespace std;
+
+char*   Jstring2CStr(JNIEnv*   env,   jstring   jstr)
+{
+    char*   rtn   =   NULL;
+    jclass   clsstring   =   env->FindClass("java/lang/String");  //String
+    jstring   strencode   =   env->NewStringUTF("GB2312"); //"gb2312"
+    jmethodID   mid   =   env->GetMethodID(clsstring,   "getBytes",   "(Ljava/lang/String;)[B"); //getBytes(Str);
+    jbyteArray   barr=   (jbyteArray)env->CallObjectMethod(jstr,mid,strencode); // String .getByte("GB2312");
+    jsize   alen   =   env->GetArrayLength(barr);
+    jbyte*   ba   =   env->GetByteArrayElements(barr,JNI_FALSE);
+    if(alen   >   0)
+    {
+        rtn   =   (char*)malloc(alen+1);         //"\0"
+        memcpy(rtn,ba,alen);
+        rtn[alen]=0;
+    }
+    env->ReleaseByteArrayElements(barr,ba,0);  //释放内存空间
+    return rtn;
+}
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_mikerush_dhy0077_wifi_1ir_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
+    string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
+}
+
+extern "C" JNIEXPORT void JNICALL
+Java_mikerush_dhy0077_wifi_1ir_MainActivity_IOT_1Sign_1MQTT(
+        JNIEnv* env,
+        jobject obj,jobject iotx_dev_meta_info,jobject iotx_sign_mqtt) {
+    jclass clazz_idev,clazz_isign;
+    jfieldID fid;jstring tmp;
+    clazz_idev=env->GetObjectClass(iotx_dev_meta_info);
+    clazz_isign=env->GetObjectClass(iotx_sign_mqtt);
+    iotx_dev_meta_info_t idev;
+    iotx_sign_mqtt_t isign;
+    //--get data from java
+    fid=env->GetFieldID(clazz_idev,"product_key","Ljava/lang/String;");
+    tmp=(jstring)env->GetObjectField(iotx_dev_meta_info,fid);
+    strcpy((char*)idev.product_key,Jstring2CStr(env,tmp));
+    fid=env->GetFieldID(clazz_idev,"product_secret","Ljava/lang/String;");
+    tmp=(jstring)env->GetObjectField(iotx_dev_meta_info,fid);
+    strcpy((char*)idev.product_secret,Jstring2CStr(env,tmp));
+    fid=env->GetFieldID(clazz_idev,"device_name","Ljava/lang/String;");
+    tmp=(jstring)env->GetObjectField(iotx_dev_meta_info,fid);
+    strcpy((char*)idev.device_name,Jstring2CStr(env,tmp));
+    fid=env->GetFieldID(clazz_idev,"device_secret","Ljava/lang/String;");
+    tmp=(jstring)env->GetObjectField(iotx_dev_meta_info,fid);
+    strcpy((char*)idev.device_secret,Jstring2CStr(env,tmp));
+    //--IOT_Sign_MQTT
+    IOT_Sign_MQTT(IOTX_CLOUD_REGION_SHANGHAI, &idev, &isign);
+    //--return value
+    fid=env->GetFieldID(clazz_isign,"hostname","Ljava/lang/String;");
+    tmp=env->NewStringUTF(isign.hostname);
+    env->SetObjectField(iotx_sign_mqtt,fid,tmp);
+    fid=env->GetFieldID(clazz_isign,"port","I");
+    env->SetIntField(iotx_sign_mqtt,fid,(int)isign.port);
+    fid=env->GetFieldID(clazz_isign,"clientid","Ljava/lang/String;");
+    tmp=env->NewStringUTF(isign.clientid);
+    env->SetObjectField(iotx_sign_mqtt,fid,tmp);
+    fid=env->GetFieldID(clazz_isign,"password","Ljava/lang/String;");
+    tmp=env->NewStringUTF(isign.password);
+    env->SetObjectField(iotx_sign_mqtt,fid,tmp);
+    fid=env->GetFieldID(clazz_isign,"username","Ljava/lang/String;");
+    tmp=env->NewStringUTF(isign.username);
+    env->SetObjectField(iotx_sign_mqtt,fid,tmp);
+}
+extern "C"
+JNIEXPORT void JNICALL
+Java_mikerush_dhy0077_wifi_1ir_MyApp_IOT_1Sign_1MQTT(JNIEnv *env, jobject thiz,
+                                                     jobject iotx_dev_meta_info,
+                                                     jobject iotx_sign_mqtt) {
+    // TODO: implement IOT_Sign_MQTT()
+    jclass clazz_idev,clazz_isign;
+    jfieldID fid;jstring tmp;
+    clazz_idev=env->GetObjectClass(iotx_dev_meta_info);
+    clazz_isign=env->GetObjectClass(iotx_sign_mqtt);
+    iotx_dev_meta_info_t idev;
+    iotx_sign_mqtt_t isign;
+    //--get data from java
+    strcpy((char*)idev.product_key,PRODUCT_KEY);
+    strcpy((char*)idev.product_secret,PRODUCT_SECERT);
+    fid=env->GetFieldID(clazz_idev,"device_name","Ljava/lang/String;");
+    tmp=(jstring)env->GetObjectField(iotx_dev_meta_info,fid);
+    strcpy((char*)idev.device_name,Jstring2CStr(env,tmp));
+    fid=env->GetFieldID(clazz_idev,"device_secret","Ljava/lang/String;");
+    tmp=(jstring)env->GetObjectField(iotx_dev_meta_info,fid);
+    strcpy((char*)idev.device_secret,Jstring2CStr(env,tmp));
+    //--IOT_Sign_MQTT
+    IOT_Sign_MQTT(IOTX_CLOUD_REGION_SHANGHAI, &idev, &isign);
+    //--return value
+    fid=env->GetFieldID(clazz_isign,"hostname","Ljava/lang/String;");
+    tmp=env->NewStringUTF(isign.hostname);
+    env->SetObjectField(iotx_sign_mqtt,fid,tmp);
+    fid=env->GetFieldID(clazz_isign,"port","I");
+    env->SetIntField(iotx_sign_mqtt,fid,(int)isign.port);
+    fid=env->GetFieldID(clazz_isign,"clientid","Ljava/lang/String;");
+    tmp=env->NewStringUTF(isign.clientid);
+    env->SetObjectField(iotx_sign_mqtt,fid,tmp);
+    fid=env->GetFieldID(clazz_isign,"password","Ljava/lang/String;");
+    tmp=env->NewStringUTF(isign.password);
+    env->SetObjectField(iotx_sign_mqtt,fid,tmp);
+    fid=env->GetFieldID(clazz_isign,"username","Ljava/lang/String;");
+    tmp=env->NewStringUTF(isign.username);
+    env->SetObjectField(iotx_sign_mqtt,fid,tmp);
 }
--- app/src/main/java/mikerush/dhy0077/wifi_ir/MainActivity.java
+++ app/src/main/java/mikerush/dhy0077/wifi_ir/MainActivity.java
@@ -1,11 +1,89 @@
 package mikerush.dhy0077.wifi_ir;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import mikerush.dhy0077.wifi_ir.ui.login.LoginActivity;
+import mikerush.dhy0077.wifi_ir.v1.EspTouchActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.widget.Button;
+import android.view.View;
 import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.hardware.usb.UsbManager;
+import android.media.Image;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.eclipse.paho.client.mqttv3.MqttException;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button start_sc_btn;
+    private Button device_info_btn;
+    private iotx_dev_meta_info_t idev;
+    private iotx_sign_mqtt_t isign;
+    private final String TAG="MainActivity";
+    private MenuItem mLogoutItem;
+    private static boolean isExit = false;
+
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            isExit = false;
+        }
+    };
+
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        Log.i(TAG, "called onCreateOptionsMenu");
+        mLogoutItem=menu.add("登出");
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Log.i(TAG, "called onOptionsItemSelected; selected item: " + item);
+        if (item == mLogoutItem) {
+            MyApp.getInstance().AutoLogin=false;
+            MyApp.getInstance().ClientLogout();
+            Intent intent=new Intent();
+            intent.setClass(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }
+        return true;
+    }
 
     // Used to load the 'native-lib' library on application startup.
     static {
@@ -16,11 +94,56 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        start_sc_btn = findViewById(R.id.start_sc);
+        start_sc_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, EspTouchActivity.class);  //从IntentActivity跳转到SubActivity
+                startActivity(intent);
+            }
+        });
+        device_info_btn = findViewById(R.id.device_info_btn);
+        device_info_btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                try {
+                    MyApp.getInstance().MQTT_Send();
+                } catch (MqttException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            exit();
+            return false;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+
+    private void exit() {
+        if (!isExit) {
+            isExit = true;
+            Toast.makeText(getApplicationContext(), "再按一次退出程序",
+                    Toast.LENGTH_SHORT).show();
+            // 利用handler延迟发送更改状态信息，2000==2秒
+            mHandler.sendEmptyMessageDelayed(0, 2000);
+        } else {
+            finish();
+            System.exit(0);
+        }
+    }
+
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
     public native String stringFromJNI();
+    public native void IOT_Sign_MQTT(iotx_dev_meta_info_t iotx_dev_meta_info,iotx_sign_mqtt_t iotx_sign_mqtt);
 }
